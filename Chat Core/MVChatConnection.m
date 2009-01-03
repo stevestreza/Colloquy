stevestreza/Colloquy

commit 274d39bef13bd208d721d83ddb7680f23b4645be

Fix a crash when deleting a connection while a room/chat is still open.
git-svn-id: http://source.colloquy.info/svn/trunk@4080 cc480944-b4dd-0310-b5e3-89908df9b951

Chat Core/MVChatConnection.m
@@ -143,6 +143,7 @@ static const NSStringEncoding supportedEncodings[] = {
 
 		CFDictionaryValueCallBacks valueCallbacks = { 0, NULL, NULL, kCFTypeDictionaryValueCallBacks.copyDescription, kCFTypeDictionaryValueCallBacks.equal };
 		_knownRooms = (NSMutableDictionary *)CFDictionaryCreateMutable(NULL, 0, &kCFTypeDictionaryKeyCallBacks, &valueCallbacks);
+		_knownUsers = (NSMutableDictionary *)CFDictionaryCreateMutable(NULL, 0, &kCFTypeDictionaryKeyCallBacks, &valueCallbacks);
 
 #if !defined(TARGET_OS_IPHONE) || !TARGET_OS_IPHONE
 		[[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self selector:@selector( _systemDidWake: ) name:NSWorkspaceDidWakeNotification object:[NSWorkspace sharedWorkspace]];
@@ -824,9 +825,9 @@ static void reachabilityCallback( SCNetworkReachabilityRef target, SCNetworkConn
 #pragma mark -
 
 - (NSSet *) knownChatUsers {
-	// subclass this method
-	[self doesNotRecognizeSelector:_cmd];
-	return nil;
+	@synchronized( _knownUsers ) {
+		return [NSSet setWithArray:[_knownUsers allValues]];
+	} return nil;
 }
 
 - (NSSet *) chatUsersWithNickname:(NSString *) nickname {
@@ -1092,6 +1093,18 @@ static void reachabilityCallback( SCNetworkReachabilityRef target, SCNetworkConn
 
 #pragma mark -
 
+- (void) _addKnownUser:(MVChatUser *) user {
+	@synchronized( _knownUsers ) {
+		if( [user uniqueIdentifier] ) [_knownUsers setObject:user forKey:[user uniqueIdentifier]];
+	}
+}
+
+- (void) _removeKnownUser:(MVChatUser *) user {
+	@synchronized( _knownUsers ) {
+		[_knownUsers removeObjectForKey:[user uniqueIdentifier]];
+	}
+}
+
 - (void) _addKnownRoom:(MVChatRoom *) room {
 	@synchronized( _knownRooms ) {
 		[_knownRooms setObject:room forKey:[room uniqueIdentifier]];
@@ -1246,7 +1259,7 @@ static void reachabilityCallback( SCNetworkReachabilityRef target, SCNetworkConn
 #pragma mark -
 
 - (NSArray *) knownChatUsersArray {
-	return [[self knownChatUsers] allObjects];
+	return [_knownUsers allValues];
 }
 
 - (MVChatUser *) valueInKnownChatUsersArrayAtIndex:(unsigned) index {

Chat Core/MVICBChatConnection.m
@@ -368,9 +368,6 @@ static BOOL hasSubstring( NSString *str, NSString *substr, NSRange *r ) {
 	if( [uniqueIdentfier isEqualToString:[_localUser uniqueIdentifier]] )
 		return [self localUser];
 
-	if( ! _knownUsers )
-		_knownUsers = [[NSMutableDictionary alloc] initWithCapacity:200];
-
 	MVChatUser *user = nil;
 	@synchronized( _knownUsers ) {
 		user = [_knownUsers objectForKey:uniqueIdentfier];
@@ -379,17 +376,9 @@ static BOOL hasSubstring( NSString *str, NSString *substr, NSRange *r ) {
 
 		user = [[MVICBChatUser alloc] initWithNickname:identifier
 		                              andConnection:self];
-		if( user )
-			[_knownUsers setObject:user forKey:uniqueIdentfier];
 	}
 
 	return [user autorelease];
-}
-
-- (NSSet *) knownChatUsers {
-	@synchronized( _knownUsers ) {
-		return [NSSet setWithArray:[_knownUsers allValues]];
-	} return nil;
 }
 
 @end

Chat Core/MVIRCChatConnection.m
@@ -145,7 +145,6 @@ static const NSStringEncoding supportedEncodings[] = {
 	[_chatConnection setDelegate:nil];
 
 	[_chatConnection release];
-	[_knownUsers release];
 	[_directClientConnections release];
 	[_server release];
 	[_currentNickname release];
@@ -167,7 +166,6 @@ static const NSStringEncoding supportedEncodings[] = {
 
 	_chatConnection = nil;
 	_connectionThread = nil;
-	_knownUsers = nil;
 	_directClientConnections = nil;
 	_server = nil;
 	_currentNickname = nil;
@@ -465,12 +463,6 @@ static const NSStringEncoding supportedEncodings[] = {
 
 #pragma mark -
 
-- (NSSet *) knownChatUsers {
-	@synchronized( _knownUsers ) {
-		return [NSSet setWithArray:[_knownUsers allValues]];
-	} return nil;
-}
-
 - (NSSet *) chatUsersWithNickname:(NSString *) name {
 	return [NSSet setWithObject:[self chatUserWithUniqueIdentifier:name]];
 }
@@ -482,16 +474,12 @@ static const NSStringEncoding supportedEncodings[] = {
 	if( [uniqueIdentfier isEqualToString:[[self localUser] uniqueIdentifier]] )
 		return [self localUser];
 
-	if( ! _knownUsers )
-		_knownUsers = [[NSMutableDictionary allocWithZone:nil] initWithCapacity:200];
-
 	MVChatUser *user = nil;
 	@synchronized( _knownUsers ) {
 		user = [_knownUsers objectForKey:uniqueIdentfier];
 		if( user ) return user;
 
 		user = [[MVIRCChatUser allocWithZone:nil] initWithNickname:identifier andConnection:self];
-		if( user ) [_knownUsers setObject:user forKey:uniqueIdentfier];
 	}
 
 	return [user autorelease];
@@ -1417,19 +1405,6 @@ end:
 
 - (void) _periodicEvents {
 	MVAssertCorrectThreadRequired( _connectionThread );
-
-	@synchronized( _knownUsers ) {
-		NSMutableArray *removeList = [[NSMutableArray allocWithZone:nil] initWithCapacity:[_knownUsers count]];
-		NSEnumerator *keyEnumerator = [_knownUsers keyEnumerator];
-		NSEnumerator *enumerator = [_knownUsers objectEnumerator];
-		id key = nil, object = nil;
-
-		while( ( key = [keyEnumerator nextObject] ) && ( object = [enumerator nextObject] ) )
-			if( [object retainCount] == 1 ) [removeList addObject:key];
-
-		[_knownUsers removeObjectsForKeys:removeList];
-		[removeList release];
-	}
 
 	@synchronized( _joinedRooms ) {
 		NSEnumerator *enumerator = [_joinedRooms objectEnumerator];

Chat Core/MVSILCChatConnection.m
@@ -933,7 +933,6 @@ static SilcClientOperations silcClientOps = {
 		[self setUsername:NSUserName()];
 		[self setRealName:NSFullUserName()];
 
-		_knownUsers = [[NSMutableDictionary allocWithZone:nil] initWithCapacity:200];
 		_queuedCommands = [[NSMutableArray allocWithZone:nil] initWithCapacity:5];
 		_sentCommands = [[NSMutableDictionary allocWithZone:nil] initWithCapacity:2];
 	}
@@ -991,9 +990,6 @@ static SilcClientOperations silcClientOps = {
 
 	[_sentCommands release];
 	_sentCommands = nil;
-
-	[_knownUsers release];
-	_knownUsers = nil;
 
 	[super dealloc];
 }
@@ -1320,10 +1316,6 @@ static void usersFoundCallback( SilcClient client, SilcClientConnection conn, Si
 
 #pragma mark -
 
-- (NSSet *) knownChatUsers {
-	return [NSSet setWithArray:[_knownUsers allValues]];
-}
-
 - (NSSet *) chatUsersWithNickname:(NSString *) findNickname {
 	if( ! [self _silcConn] ) return nil;
 
@@ -1378,10 +1370,8 @@ static void usersFoundCallback( SilcClient client, SilcClientConnection conn, Si
 			SilcLock( [self _silcClient] );
 			SilcClientEntry client = silc_client_get_client_by_id( [self _silcClient], [self _silcConn], clientID );
 			SilcUnlock( [self _silcClient] );
-			if( client ) {
+			if( client )
 				user = [[MVSILCChatUser allocWithZone:nil] initWithClientEntry:client andConnection:self];
-				if( user ) [_knownUsers setObject:user forKey:data];
-			}
 		}
 	}
 
@@ -1560,14 +1550,6 @@ static void usersFoundCallback( SilcClient client, SilcClientConnection conn, Si
 
 #pragma mark -
 
-- (void) _removeKnownUser:(MVChatUser *) user {
-	@synchronized( _knownUsers ) {
-		if( user ) [_knownUsers removeObjectForKey:[user uniqueIdentifier]];
-	}
-}
-
-#pragma mark -
-
 - (void) _setDetachInfo:(NSData *) info {
 	@synchronized( _persistentInformation ) {
 		if( info ) [_persistentInformation setObject:info forKey:@"detachData"];
@@ -1632,7 +1614,6 @@ static void usersFoundCallback( SilcClient client, SilcClientConnection conn, Si
 		[self scheduleReconnectAttempt];
 	}
 
-	[_knownUsers removeAllObjects];
 	[_sentCommands removeAllObjects];
 	[_queuedCommands removeAllObjects];
 
@@ -1670,7 +1651,6 @@ static void usersFoundCallback( SilcClient client, SilcClientConnection conn, Si
 		if( user ) return user;
 
 		user = [[MVSILCChatUser allocWithZone:nil] initWithClientEntry:clientEntry andConnection:self];
-		if( user ) [_knownUsers setObject:user forKey:uniqueIdentfier];
 	}
 
 	return [user autorelease];

Chat Core/MVXMPPChatConnection.m
@@ -53,7 +53,6 @@
 	[_username release];
 	[_nickname release];
 	[_password release];
-	[_knownUsers release];
 
 	_session = nil;
 	_localID = nil;
@@ -61,7 +60,6 @@
 	_username = nil;
 	_nickname = nil;
 	_password = nil;
-	_knownUsers = nil;
 
 	[super dealloc];
 }
@@ -193,12 +191,6 @@
 
 #pragma mark -
 
-- (NSSet *) knownChatUsers {
-	@synchronized( _knownUsers ) {
-		return [NSSet setWithArray:[_knownUsers allValues]];
-	} return nil;
-}
-
 - (NSSet *) chatUsersWithNickname:(NSString *) name {
 	return [NSSet setWithObject:[self chatUserWithUniqueIdentifier:name]];
 }
@@ -211,16 +203,12 @@
 	if( [identifier isEqual:[[self localUser] uniqueIdentifier]] )
 		return [self localUser];
 
-	if( ! _knownUsers )
-		_knownUsers = [[NSMutableDictionary allocWithZone:nil] initWithCapacity:50];
-
 	MVChatUser *user = nil;
 	@synchronized( _knownUsers ) {
 		user = [_knownUsers objectForKey:[identifier completeID]];
 		if( user ) return user;
 
 		user = [[MVXMPPChatUser allocWithZone:nil] initWithJabberID:identifier andConnection:self];
-		if( user ) [_knownUsers setObject:user forKey:[identifier completeID]];
 	}
 
 	return [user autorelease];

Chat Core/MVChatConnection.h
@@ -104,6 +104,7 @@ extern NSString *MVChatConnectionErrorDomain;
 	NSStringEncoding _encoding;
 
 	NSString *_npassword;
+	NSMutableDictionary *_knownUsers;
 	NSMutableDictionary *_knownRooms;
 	NSMutableSet *_joinedRooms;
 	MVChatUser *_localUser;

Chat Core/MVChatConnectionPrivate.h
@@ -16,6 +16,9 @@
 - (void) _postError:(NSError *) error;
 - (void) _setStatus:(MVChatConnectionStatus) status;
 
+- (void) _addKnownUser:(MVChatUser *) user;
+- (void) _removeKnownUser:(MVChatUser *) user;
+
 - (void) _addKnownRoom:(MVChatRoom *) room;
 - (void) _removeKnownRoom:(MVChatRoom *) room;
 

Chat Core/MVChatRoom.m
@@ -83,8 +83,9 @@ NSString *MVChatRoomAttributeUpdatedNotification = @"MVChatRoomAttributeUpdatedN
 	[_bannedUsers release];
 	[_modeAttributes release];
 	[_memberModes release];
+	[_connection release];
 
-	_connection = nil; // connection isn't retained, prevents circular retain
+	_connection = nil;
 	_name = nil;
 	_uniqueIdentifier = nil;
 	_dateJoined = nil;

commit 0868718a95b30e5de68aa34bd8079f3d93cd3e2b

Fix a crash caused by r4080.
git-svn-id: http://source.colloquy.info/svn/trunk@4084 cc480944-b4dd-0310-b5e3-89908df9b951

Chat Core/MVChatConnection.m
@@ -774,7 +774,7 @@ static void reachabilityCallback( SCNetworkReachabilityRef target, SCNetworkConn
 - (MVChatRoom *) joinedChatRoomWithUniqueIdentifier:(id) identifier {
 	@synchronized( _joinedRooms ) {
 		MVChatRoom *room = [_knownRooms objectForKey:identifier];
-		return ([room isJoined] ? room : nil);
+		return ([room isJoined] ? [[room retain] autorelease] : nil);
 	} return nil;
 }
 
@@ -785,7 +785,7 @@ static void reachabilityCallback( SCNetworkReachabilityRef target, SCNetworkConn
 
 		while( ( room = [enumerator nextObject] ) )
 			if( [[room name] isEqualToString:name] )
-				return room;
+				return [[room retain] autorelease];
 	}
 
 	return nil;
@@ -795,7 +795,7 @@ static void reachabilityCallback( SCNetworkReachabilityRef target, SCNetworkConn
 
 - (MVChatRoom *) chatRoomWithUniqueIdentifier:(id) identifier {
 	@synchronized( _knownRooms ) {
-		return [_knownRooms objectForKey:identifier];
+		return [[[_knownRooms objectForKey:identifier] retain] autorelease];
 	} return nil;
 }
 
@@ -806,7 +806,7 @@ static void reachabilityCallback( SCNetworkReachabilityRef target, SCNetworkConn
 
 		while( ( room = [enumerator nextObject] ) )
 			if( [[room name] isEqualToString:name] )
-				return room;
+				return [[room retain] autorelease];
 	}
 
 	return nil;

Chat Core/MVICBChatConnection.m
@@ -372,7 +372,7 @@ static BOOL hasSubstring( NSString *str, NSString *substr, NSRange *r ) {
 	@synchronized( _knownUsers ) {
 		user = [_knownUsers objectForKey:uniqueIdentfier];
 		if( user )
-			return user;
+			return [[user retain] autorelease];
 
 		user = [[MVICBChatUser alloc] initWithNickname:identifier
 		                              andConnection:self];

Chat Core/MVIRCChatConnection.m
@@ -477,7 +477,7 @@ static const NSStringEncoding supportedEncodings[] = {
 	MVChatUser *user = nil;
 	@synchronized( _knownUsers ) {
 		user = [_knownUsers objectForKey:uniqueIdentfier];
-		if( user ) return user;
+		if( user ) return [[user retain] autorelease];
 
 		user = [[MVIRCChatUser allocWithZone:nil] initWithNickname:identifier andConnection:self];
 	}

Chat Core/MVSILCChatConnection.m
@@ -1648,7 +1648,7 @@ static void usersFoundCallback( SilcClient client, SilcClientConnection conn, Si
 	MVChatUser *user = nil;
 	@synchronized( _knownUsers ) {
 		user = [_knownUsers objectForKey:uniqueIdentfier];
-		if( user ) return user;
+		if( user ) return [[user retain] autorelease];
 
 		user = [[MVSILCChatUser allocWithZone:nil] initWithClientEntry:clientEntry andConnection:self];
 	}

Chat Core/MVXMPPChatConnection.m
@@ -206,7 +206,7 @@
 	MVChatUser *user = nil;
 	@synchronized( _knownUsers ) {
 		user = [_knownUsers objectForKey:[identifier completeID]];
-		if( user ) return user;
+		if( user ) return [[user retain] autorelease];
 
 		user = [[MVXMPPChatUser allocWithZone:nil] initWithJabberID:identifier andConnection:self];
 	}